b918de00c13fbd7187067e4aa33fbdd529be59a5
Zachary King <zacharyking@zacharys-mbp.uconnect.utah.edu>
2017-10-27 19:10:25 -0600

    Initial Commit

---
+++ SpeechTotext/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechTotext
+//
+//  Created by Zachary King on 10/27/17.
+//  Copyright © 2017 Zachary King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
